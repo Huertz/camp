a1803874e950807195ea3b28a9909bb346fdbfb9
Huertz <104065687+Huertz@users.noreply.github.com>
2023-06-24 04:06:10 -0500

    created basic CRUD

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/camp', {
@@ -20,15 +21,62 @@ const app = express();
 app.set('view engine', 'ejs');
 //! directly connects to the view folder
 app.set('views', path.join(__dirname, 'views'));
+//! to parse body from req
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 //! renders the page on views
 app.get('/', (req, res) => {
   res.render('home');
 });
 
+//! show a campgrounds
 app.get('/campgrounds', async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', { campgrounds });
+});
+
+//! order does matter
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+
+//! creates campground
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  //! redirect mostly used in POST
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+//! camp by id
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/show', { campground });
+});
+
+//! edit camp by id
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit', { campground });
+});
+
+//! updates camp by id
+//! can also used put
+app.patch('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  //! redirect mostly used in POST
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+//! deletes camp by id for now..
+app.delete('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect('/campgrounds');
 });
 
 app.listen(3000, () => {
