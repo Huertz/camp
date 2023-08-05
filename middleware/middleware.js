43ba5e76e5034d896cf1a7d4873952dc6d006c9c
Huertz <104065687+Huertz@users.noreply.github.com>
2023-08-05 13:38:51 -0500

    added middleware to redirect login correct page

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,5 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    //! store url that are requesting
+    req.session.returnTo = req.originalUrl; // add this line
     req.flash('error', 'Must be signed in');
     return res.redirect('/login');
   }
