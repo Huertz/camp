f62a760407bf164c00f9626e34f564dc6041a2d5
Huertz <104065687+Huertz@users.noreply.github.com>
2023-08-07 00:01:59 -0500

    reviews controllers

--- routes/reviews.js
+++ routes/reviews.js
@@ -12,36 +12,18 @@ const ExpressError = require('../utils/ExpressError');
 //? schema setup
 const Campground = require('../models/campground');
 const Review = require('../models/review');
+//? controllers
+const reviews = require('../controllers/reviews');
 
 //? creates review in campground
-router.post(
-  '/',
-  isLoggedIn,
-  validateReview,
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Created a review');
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
-);
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
 //? deletes reviews
 router.delete(
   '/:reviewId',
   isLoggedIn,
   isReviewAuthor,
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, { $pull: { review: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Review deleted');
-    res.redirect(`/campgrounds/${id}`);
-  })
+  catchAsync(reviews.deleteReview)
 );
 
 module.exports = router;
