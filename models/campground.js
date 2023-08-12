923bd85e19257efb02ed090cfd72173241597eb6
Huertz <104065687+Huertz@users.noreply.github.com>
2023-08-12 15:07:42 -0500

    thumbnail images in edit

--- models/campground.js
+++ models/campground.js
@@ -3,14 +3,19 @@ const Review = require('./review');
 const Schema = mongoose.Schema;
 
 //! schema
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+//! to reduce images to thumbnails
+ImageSchema.virtual('small').get(function () {
+  return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampSchema = new Schema({
   title: String,
-  images: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
