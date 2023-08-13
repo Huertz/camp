ad1a0864ba8b2d71ef708d2251f2024bea6f35c3
Huertz <104065687+Huertz@users.noreply.github.com>
2023-08-12 21:13:01 -0500

    issue wit show.ejs

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,8 +1,9 @@
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiaHVlcnR6IiwiYSI6ImNsbDhtaHExZTA0Z3IzZ3RlcGJ1dmllMTQifQ.Km2el4IYqjGTy58LFEIRyQ';
+mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v12', // style URL
   center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 4, // starting zoom
+  zoom: 9, // starting zoom
 });
+
+new mapboxgl.Marker().setLngLat([-74.5, 40]).addTo(map);
