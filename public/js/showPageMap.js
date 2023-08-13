ed8a33dc949d1025b625eaef9681f83b7c679d1c
Huertz <104065687+Huertz@users.noreply.github.com>
2023-08-12 19:29:28 -0500

    map displayed

---
+++ public/js/showPageMap.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiaHVlcnR6IiwiYSI6ImNsbDhtaHExZTA0Z3IzZ3RlcGJ1dmllMTQifQ.Km2el4IYqjGTy58LFEIRyQ';
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 4, // starting zoom
+});
