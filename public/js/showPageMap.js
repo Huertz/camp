cf498eef835bd52cad08ed6fa9ab84bd19469c03
Huertz <104065687+Huertz@users.noreply.github.com>
2023-10-12 20:38:08 -0500

    actually fiex the issue with map showing the page

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,10 +1,9 @@
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiaHVlcnR6IiwiYSI6ImNsbDhtaHExZTA0Z3IzZ3RlcGJ1dmllMTQifQ.Km2el4IYqjGTy58LFEIRyQ';
+mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v12', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
+  center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 9, // starting zoom
 });
 
-new mapboxgl.Marker().setLngLat([-74.5, 40]).addTo(map);
+new mapboxgl.Marker().setLngLat(campground.geometry.coordinates).addTo(map);
